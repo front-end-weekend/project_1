e33947fdc1d8f47a764471da7f9044f88a79f9c8
ohwphil <ohwphil@gmail.com>
2023-03-11 00:53:06 +0900

    feat: 드랍다운 선택 js, 모달창 선택 js 추가(미완성)

--- project_페어3/projects.js
+++ project_페어3/projects.js
@@ -21,21 +21,56 @@ document
   });
 /* 드랍다운 선택시키기 */
 const workFormLis = document.querySelectorAll(".work_form .dropdown > *");
-console.log(Array.from(workFormLis));
-const removeAllSelected = function () {
-  Array.from(workFormLis).forEach((v) => {
+const workFormText = document.querySelector(".work_form .criterion_value");
+const removeAllSelected = function (lis) {
+  Array.from(lis).forEach((v) => {
     v.classList.remove("selected");
   });
 };
 Array.from(workFormLis).forEach((v, i) => {
+  v.innerHTML += "<div>✓</div>";
   v.addEventListener("click", function () {
-    console.log(v);
-    console.log("안녕");
-    removeAllSelected();
+    removeAllSelected(workFormLis);
     workFormLis[i].classList.add("selected");
+    workFormText.innerHTML = document.querySelector(
+      ".work_form li.selected div"
+    ).innerHTML;
   });
 });
-
+/* 직군/직무 모달창 내부 구현 */
+const firstModalButton1 = document.querySelector("#work_type_modal .area");
+const firstModalButton2 = document.querySelector("#work_type_modal .task");
+const firstModalDropdowns = document.querySelectorAll(
+  "#work_type_modal .dropdown"
+);
+firstModalButton1.addEventListener("click", function () {
+  firstModalDropdowns[0].classList.toggle("hidden");
+});
+/* 첫 드랍다운 선택 시 토글됨 */
+Array.from(firstModalDropdowns[0].children).forEach((v, i) => {
+  v.innerHTML += "<div>✓</div>";
+  v.addEventListener("click", function () {
+    removeAllSelected(firstModalDropdowns[0].children);
+    firstModalDropdowns[0].children[i].classList.add("selected");
+    firstModalButton1.children[0].innerHTML =
+      firstModalDropdowns[0].children[i].children[0].innerHTML;
+    firstModalButton2.classList.remove("hidden");
+    firstModalDropdowns[1].classList.toggle("hidden");
+  });
+});
+firstModalButton2.addEventListener("click", function () {
+  firstModalDropdowns[1].classList.toggle("hidden");
+});
+Array.from(firstModalDropdowns[1].children).forEach((v, i) => {
+  v.innerHTML += "<div>✓</div>";
+  v.addEventListener("click", function () {
+    // firstModalButton1.children[1].innerHTML =
+    //   firstModalDropdowns[1].children[i].children[0].innerHTML;
+    firstModalDropdowns[1].children[i].classList.toggle("selected");
+    firstModalButton2.classList.toggle("selected");
+    // 내일 이 문제 해결하기
+  });
+});
 /* 모달창 닫기 */
 const close_btn = document.getElementsByClassName("close");
 Array.from(close_btn).forEach((v, i) => {
